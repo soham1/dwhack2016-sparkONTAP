d47bd202540ad681404f9382f6168c0d8ed5e8b5
Kaushik Ashodiya <kaushika@hotmail.com>
2016-02-14 04:57:21 +0000

    new route

--- routes/redisroute.js
+++ routes/redisroute.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('redis');
+var client = redis.createClient(16022, 'pub-redis-16022.us-east-1-3.7.ec2.redislabs.com', {
+  no_ready_check: true
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
